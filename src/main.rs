967225f7c46787f5b2fd34c2a27e325862127341
Jagannath Natarajan <jagnat4000@gmail.com>
2018-06-21 16:43:53 -0700

    Learn simple image processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+extern crate image;
+
+use image::Rgba;
+use image::ImageBuffer;
+
+fn main()
+{
+	let mut img : ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(512, 512);
+	for x in 0..512
+	{
+		for y in 0..512
+		{
+			img.put_pixel(x, y, Rgba([0, 0, 59, 255]));
+		}
+	}
+	let img_write_res = img.save("test.png").unwrap();
 }
+
